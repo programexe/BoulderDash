dff9f7c3528a8edd57b3a95651c70276da3516ab
programexe <humarezequiel@gmail.com>
2017-10-22 21:33:33 -0300

    Agregue para que pueda mover las piedras

--- src/Juego/test.java
+++ src/Juego/test.java
@@ -1,11 +1,11 @@
 package Juego;
-
+import CargaDeNiveles.*;
 public class test {
 	public static void main(String[] args){
 		Mapa m=new Mapa();
 		m.crearMapa();
 		int lvl=0;
-		
+		m.getLector().readLevels(levels.xml);
 		m.getLector().setCurrentLevel(lvl);
 	}
 	
